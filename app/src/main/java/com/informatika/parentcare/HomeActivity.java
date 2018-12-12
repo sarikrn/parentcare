c4e868a37d5f1faab494cd55d897ce45dd92abc0
sarikrn <16523060@students.uii.ac.id>
2018-12-12 10:28:38 +0700

    linking

--- app/src/main/java/com/informatika/parentcare/HomeActivity.java
+++ app/src/main/java/com/informatika/parentcare/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.informatika.parentcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void DetailChildInfo(View view) {
+        Intent intent = new Intent(HomeActivity.this, ChildProfileActivity.class);
+        startActivity(intent);
     }
 }
